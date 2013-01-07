62df8c3073982537d721445b8b0acb49bf32b224
khamilto <khamilto@10.40.13.211>
2013-01-07 11:29:40 -0500

    stuff from work computer

---
+++ corpid.php
@@ -0,0 +1,6 @@
+<?php 
+$title = "copy &amp; paste generation - portfolio";
+$body_id = "portfolio";
+require_once "header.php";
+require_once "corpid-content.php";       
+require_once "footer.php";

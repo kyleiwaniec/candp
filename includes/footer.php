d3cf284ee8c48e1810321f67ed379da07bf20feb
kyleiwaniec <kyleih@optonline.net>
2013-01-08 09:15:33 -0500

    remove placeholder script

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
         <script src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
         <script src="/js/raphael-min.js"></script>
         <script src="/js/plugins.js"></script>
-        <script src="/js/jquery.placeholder-enhanced.js"></script>
+       <!-- <script src="/js/jquery.placeholder-enhanced.js"></script> -->
         <script src="/js/jquery.validate.js"></script>
         <script src="/js/scripts.js"></script>
         <script src="/js/mobile-menu.js"></script>
